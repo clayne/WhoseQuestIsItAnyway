5661fb34792c5475419b1b2568e29089ed37ebcb
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2019-07-25 22:07:23 -0700

    Update Settings.cpp

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -3,10 +3,10 @@
 
 bool Settings::loadSettings(bool a_dumpParse)
 {
-	return Json2Settings::Settings::loadSettings(FILE_NAME, false, a_dumpParse);
+	return Json2Settings::Settings::loadSettings(FILE_NAME, a_dumpParse);
 }
 
 
-decltype(Settings::printQuestFormID)	Settings::printQuestFormID("printQuestFormID", false, false);
-decltype(Settings::useRandomMessages)	Settings::useRandomMessages("useRandomMessages", false, false);
-decltype(Settings::totalMessageCount)	Settings::totalMessageCount("totalMessageCount", false, false);
+decltype(Settings::printQuestFormID)	Settings::printQuestFormID("printQuestFormID", false);
+decltype(Settings::useRandomMessages)	Settings::useRandomMessages("useRandomMessages", false);
+decltype(Settings::totalMessageCount)	Settings::totalMessageCount("totalMessageCount", false);
